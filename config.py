e541e9c69d59c634117815efdf19f0767cc0788d
Namita Chougule <namitac9817@gmail.com>
2025-12-23 19:35:31 -0500

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# Supabase project URL
-SUPABASE_URL = "https://scpyjqvboabszqvtkqfa.supabase.co" 
-# Supabase service_role key
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjcHlqcXZib2Fic3pxdnRrcWZhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2NTIxMjE3NSwiZXhwIjoyMDgwNzg4MTc1fQ.Lveh9EfobMsjOmAFAPfwd1fhLN_Dv8L6fjvKPCIf1qc"
-
-OPENAI_API_KEY = ""
-
-USE_AI = False 
